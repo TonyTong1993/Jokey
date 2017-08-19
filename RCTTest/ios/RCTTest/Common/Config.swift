8e2898783e67c84cf6c2977ec708db4ca5fda573
童万华 <tongwanhua@tongwanhuademac.local>
2017-08-18 22:06:06 -0700

    添加swift配置文件，及补充颜色

--- RCTTest/ios/RCTTest/Common/Config.swift
+++ RCTTest/ios/RCTTest/Common/Config.swift
@@ -7,3 +7,8 @@
 //
 
 import Foundation
+
+extension UIColor {
+   @nonobjc static let textTint     = UIColor.hex(0xfbfbfb)
+   @nonobjc static let barTint      = UIColor.hex(0x010101)
+}
---
+++ RCTTest/ios/RCTTest/Global/Theme/TYTheme.m
@@ -0,0 +1,46 @@
+//
+//  TYTheme.m
+//  RCTTest
+//
+//  Created by 童万华 on 2017/7/7.
+//  Copyright © 2017年 童万华. All rights reserved.
+//
+//导航栏选中字体颜色 5BB4EB 未选中颜色 black
+
+#import "TYTheme.h"
+NSInteger const themeColorHexValue = 0x55B1E6;
+NSInteger const separatorColorHexValue = 0xebebeb;
+@implementation TYTheme
+static NSString *themeColor;
++(NSString *)themeColorWithType:(TYThemeType)type {
+    NSString *color;
+    switch (type) {
+        case TYThemeLight:
+            color = @"#e0ffff";
+            break;
+            
+        case TYThemeDark:
+             color = @"#333333";
+            break;
+            
+        case TYThemeBrightRed:
+             color = @"#dc143c";
+            break;
+        case TYThemeSkyBlue:
+             color = @"#87ceeb";
+            break;
+        case TYThemePureWhite:
+            color = @"#ffffff";
+            break;
+    }
+    themeColor = color;
+    return color;
+}
++(NSString *)themeColor {
+    return themeColor;
+}
++(NSString *)themeFontFamilyName {
+    return @"PingFangSC-Regular";
+}
+
+@end
