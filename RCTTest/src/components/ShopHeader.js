0f620d15ba446e8b96553b6d06f20e69d17f1eaf
tonytong <tongwanhua1993@163.com>
2017-08-30 15:25:36 +0800

    更新轮播图

--- RCTTest/src/components/ShopHeader.js
+++ RCTTest/src/components/ShopHeader.js
@@ -2,18 +2,34 @@ import React from 'react'
 import {
   StyleSheet,
   View,
+  Image,
   ScrollView,
-  Text
+  Text,
+  Dimensions
 } from 'react-native'
-
+const data = [
+              "http://img61.ddimg.cn/upload_img/00705/yhj4/640x284_lyx_0829.jpg",
+              "http://img63.ddimg.cn/upload_img/00657/830/Touch-6.6-1.jpg",
+              "http://img54.ddimg.cn/192030035267244_y.jpg"
+            ]
 export default class ReCycleView extends React.Component{
     render() {
       return (
         <ScrollView
           contentContainerStyle={styles.container}
           horizontal={true}
+          pagingEnabled={true}
+          showsHorizontalScrollIndicator={false}
           >
-          <Text >头部</Text>
+         <Image
+           style={styles.RecycleImage}
+          source={{uri:data[0]}} />
+          <Image
+            style={styles.RecycleImage}
+           source={{uri:data[1]}} />
+           <Image
+             style={styles.RecycleImage}
+            source={{uri:data[2]}} />
         </ScrollView>
       )
     }
@@ -21,10 +37,14 @@ export default class ReCycleView extends React.Component{
 
 var styles = StyleSheet.create({
   container: {
-    flex:1,
-    height:120,
+    width:Dimensions.get('window').width*3,
+    height:166.41,
     backgroundColor:'#ffb300',
     justifyContent:'center',
     alignItems:'center'
+  },
+  RecycleImage:{
+    width:Dimensions.get('window').width,
+    height:166.41
   }
 })
