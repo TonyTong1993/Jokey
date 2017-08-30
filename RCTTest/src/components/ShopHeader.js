36020c6dbcba6f28c59080a212603c8afa124960
tonytong <tongwanhua1993@163.com>
2017-08-30 17:16:16 +0800

    更新定时滚动轮播图

--- RCTTest/src/components/ShopHeader.js
+++ RCTTest/src/components/ShopHeader.js
@@ -6,20 +6,48 @@ import {
   ScrollView,
   Text,
   Dimensions
-} from 'react-native'
+} from 'react-native';
+const width = Dimensions.get('window').width;
 const data = [
               "http://img61.ddimg.cn/upload_img/00705/yhj4/640x284_lyx_0829.jpg",
               "http://img63.ddimg.cn/upload_img/00657/830/Touch-6.6-1.jpg",
               "http://img54.ddimg.cn/192030035267244_y.jpg"
-            ]
+            ];
 export default class ReCycleView extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      page:0
+    }
+  }
+  componentDidMount(){
+    //开启计时器
+    var scrollView = this.refs.scrollView
+
+    this.timer = setInterval(()=>{
+      var activePage = this.state.page+1;
+
+      this.setState({
+        page:activePage%3
+      })
+       scrollView.scrollResponderScrollTo({x:this.state.page*width,y:0,animated:true})
+    },5000)
+
+  }
+  componentWillUnmount() {
+     this.timer && clearInterval(this.timer);
+  }
     render() {
+
       return (
         <ScrollView
+          ref='scrollView'
           contentContainerStyle={styles.container}
           horizontal={true}
           pagingEnabled={true}
           showsHorizontalScrollIndicator={false}
+          automaticallyAdjustContentInsets={false}
           >
          <Image
            style={styles.RecycleImage}
@@ -38,7 +66,7 @@ export default class ReCycleView extends React.Component{
 var styles = StyleSheet.create({
   container: {
     width:Dimensions.get('window').width*3,
-    height:166.41,
+    height:166.41,//fixme:需做尺寸适配
     backgroundColor:'#ffb300',
     justifyContent:'center',
     alignItems:'center'
