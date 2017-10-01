17ae19f731036700ca3731ddefe06d7f104ff6cf
TWH-PC\TonyTong <tongwanhua1993@163.com>
2017-10-01 20:09:30 +0800

    更新codepush

--- RCTTest/src/app.js
+++ RCTTest/src/app.js
@@ -6,7 +6,38 @@ import {
   View,
   Text,
 } from 'react-native';
-
+import CodePush from 'react-native-code-push' 
 import App from './RouteConfig.js'
+let codePushOptions = { checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME };
+function syncImmediate () {
+  CodePush.sync(
 
-export default App;
+             { installMode: CodePush.InstallMode.IMMEDIATE,//启动模式三种：ON_NEXT_RESUME、ON_NEXT_RESTART、IMMEDIATE
+
+                updateDialog: {
+
+                        appendReleaseDescription:true,//是否显示更新description，默认为false
+
+                        descriptionPrefix:"更新内容：",//更新说明的前缀。 默认是” Description:
+
+                        mandatoryContinueButtonLabel:"立即更新",//强制更新的按钮文字，默认为continue
+
+                        mandatoryUpdateMessage:"",//- 强制更新时，更新通知. Defaults to “An update is available that must be installed.”.
+
+                        optionalIgnoreButtonLabel: '稍后',//非强制更新时，取消按钮文字,默认是ignore
+
+                       optionalInstallButtonLabel: '后台更新',//非强制更新时，确认文字. Defaults to “Install”
+
+                      optionalUpdateMessage: '有新版本了，是否更新？',//非强制更新时，更新通知. Defaults to “An update is available. Would you like to install it?”.
+
+                      title: '更新提示'//要显示的更新通知的标题. Defaults to “Update available”.
+
+                  },
+
+            },
+
+        );
+}
+syncImmediate();
+let MyApp = CodePush(codePushOptions)(App);
+export default MyApp;
--- RCTTest/src/pages/HomePage.js
+++ RCTTest/src/pages/HomePage.js
@@ -10,7 +10,6 @@ import {
   Button,
   TouchableOpacity,
 } from 'react-native';
-import CodePush from 'react-native-code-push' 
 import ReCycleView from '../components/ShopHeader'
 import SearchHeader from '../components/SearchHeader'
 import ShopFooter from '../components/ShopFooter'
@@ -18,7 +17,7 @@ import RowBanner from '../components/RowBanner'
 import ThirdBanner from '../components/ThirdBanner'
 import {screen_width,screen_height} from '../Config'
 
-let codePushOptions = { checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME };
+
 
 const data = [
               {key:'http://img63.ddimg.cn/upload_img/00721/zjl/640x342_wzh_20170830.jpg'},
@@ -64,12 +63,6 @@ class HomePage extends Component {
       </View>
     );
   }
-  componentWillMount() {
-       CodePush.disallowRestart();//页面加载的禁止重启，在加载完了可以允许重启
-  }
-componentDidMount() {
-    CodePush.allowRestart();//在加载完了可以允许重启
-}
   /*private method*/
   _renderItem =({item,index}) =>{
     console.log(item.key)
@@ -141,35 +134,6 @@ const styles = StyleSheet.create({
     backgroundColor:'#ffb300'
   }
 });
-function syncImmediate () {
-  CodePush.sync(
 
-             { installMode: CodePush.InstallMode.IMMEDIATE,//启动模式三种：ON_NEXT_RESUME、ON_NEXT_RESTART、IMMEDIATE
 
-                updateDialog: {
-
-                        appendReleaseDescription:true,//是否显示更新description，默认为false
-
-                        descriptionPrefix:"更新内容：",//更新说明的前缀。 默认是” Description:
-
-                        mandatoryContinueButtonLabel:"立即更新",//强制更新的按钮文字，默认为continue
-
-                        mandatoryUpdateMessage:"",//- 强制更新时，更新通知. Defaults to “An update is available that must be installed.”.
-
-                        optionalIgnoreButtonLabel: '稍后',//非强制更新时，取消按钮文字,默认是ignore
-
-                       optionalInstallButtonLabel: '后台更新',//非强制更新时，确认文字. Defaults to “Install”
-
-                      optionalUpdateMessage: '有新版本了，是否更新？',//非强制更新时，更新通知. Defaults to “An update is available. Would you like to install it?”.
-
-                      title: '更新提示'//要显示的更新通知的标题. Defaults to “Update available”.
-
-                  },
-
-            },
-
-        );
-}
-syncImmediate();
-let MyApp = CodePush(codePushOptions)(HomePage);
-export default MyApp;
+export default HomePage;
