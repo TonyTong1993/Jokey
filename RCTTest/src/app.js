0f213b4dc48da3f269f087fe9693c288f51380bd
TWH-PC\TonyTong <tongwanhua1993@163.com>
2017-10-01 19:15:21 +0800

    更新codepush为立即更新

--- RCTTest/src/app.js
+++ RCTTest/src/app.js
@@ -6,8 +6,7 @@ import {
   View,
   Text,
 } from 'react-native';
-import codePush from 'react-native-code-push' 
+
 import App from './RouteConfig.js'
-let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
-let MyApp = codePush(codePushOptions)(App);
-export default MyApp;
+
+export default App;
--- RCTTest/src/pages/HomePage.js
+++ RCTTest/src/pages/HomePage.js
@@ -10,13 +10,15 @@ import {
   Button,
   TouchableOpacity,
 } from 'react-native';
-
+import CodePush from 'react-native-code-push' 
 import ReCycleView from '../components/ShopHeader'
 import SearchHeader from '../components/SearchHeader'
 import ShopFooter from '../components/ShopFooter'
 import RowBanner from '../components/RowBanner'
 import ThirdBanner from '../components/ThirdBanner'
 import {screen_width,screen_height} from '../Config'
+
+let codePushOptions = { checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME };
 
 const data = [
               {key:'http://img63.ddimg.cn/upload_img/00721/zjl/640x342_wzh_20170830.jpg'},
@@ -46,8 +48,8 @@ class HomePage extends Component {
   render() {
     return (
        <View style={styles.container}>
-          <FlatList
-           automaticallyAdjustContentInsets={false}
+        <FlatList
+          automaticallyAdjustContentInsets={false}
            contentContainerStyle={{paddingBottom:49,paddingTop:0}}
            data={data}
            ListHeaderComponent={this._ListHeaderComponent}
@@ -58,12 +60,15 @@ class HomePage extends Component {
             console.log('onEndReached')
            }}
            onEndReachedThreshold={0.5}
-         />
+           />
       </View>
     );
   }
+  componentWillMount() {
+       CodePush.disallowRestart();//页面加载的禁止重启，在加载完了可以允许重启
+  }
 componentDidMount() {
-  
+    CodePush.allowRestart();//在加载完了可以允许重启
 }
   /*private method*/
   _renderItem =({item,index}) =>{
@@ -136,6 +141,35 @@ const styles = StyleSheet.create({
     backgroundColor:'#ffb300'
   }
 });
+function syncImmediate () {
+  CodePush.sync(
 
+             { installMode: CodePush.InstallMode.IMMEDIATE,//启动模式三种：ON_NEXT_RESUME、ON_NEXT_RESTART、IMMEDIATE
 
-export default HomePage;
+                updateDialog: {
+
+                        appendReleaseDescription:true,//是否显示更新description，默认为false
+
+                        descriptionPrefix:"更新内容：",//更新说明的前缀。 默认是” Description:
+
+                        mandatoryContinueButtonLabel:"立即更新",//强制更新的按钮文字，默认为continue
+
+                        mandatoryUpdateMessage:"",//- 强制更新时，更新通知. Defaults to “An update is available that must be installed.”.
+
+                        optionalIgnoreButtonLabel: '稍后',//非强制更新时，取消按钮文字,默认是ignore
+
+                       optionalInstallButtonLabel: '后台更新',//非强制更新时，确认文字. Defaults to “Install”
+
+                      optionalUpdateMessage: '有新版本了，是否更新？',//非强制更新时，更新通知. Defaults to “An update is available. Would you like to install it?”.
+
+                      title: '更新提示'//要显示的更新通知的标题. Defaults to “Update available”.
+
+                  },
+
+            },
+
+        );
+}
+syncImmediate();
+let MyApp = CodePush(codePushOptions)(HomePage);
+export default MyApp;
