27db028eb164baeb3a1ab0ec92479f990cfdd18c
tonytong <tongwanhua1993@163.com>
2017-12-04 11:45:23 +0800

    添加错误分析总结

---
+++ RCTTest/ios/CrashAnalysis/CrashAnalysis/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  CrashAnalysis
+//
+//  Created by 童万华 on 2017/12/4.
+//  Copyright © 2017年 童万华. All rights reserved.
+/*
+     获取错误日子方式：
+     怎么分析错误日子：
+ */
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+      
+    }
+    return 0;
+}
